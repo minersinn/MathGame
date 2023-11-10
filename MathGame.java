e98bfc014a23b79c037bd626677f4faf0151385b
minersinn <rglm14@gmail.com>
2023-11-10 16:04:34 -0500

    Updated flow to game
    Functions are incomplete. Game will not run yet.

--- MathGame.java
+++ MathGame.java
@@ -1,136 +1,136 @@
-import java.util.Scanner; // Import the Scanner class
-import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
 
 public class MathGame {
 
+    static Scanner myScanner = new Scanner(System.in); // Create a Scanner object
+    static byte lives = 3;
+    static byte correctAnswers = 0;
+    static String difficultyLevel; // Variable for user menu selection
+    static int number1;
+    static int number2;
+    static int answer;
+    static int userAnswer;
+    static String userName;
+    
     public static void main(String[] args) {
-        Scanner scannerBoi = new Scanner(System.in); // Create a Scanner object
-        String operator = ""; // Variable for user menu selection
-        int number1 = 0;
-        int number2 = 0;
-        int answer = 0;
-        int userAnswer = 0;
-        byte lives = 3;
-        byte correctAnswers = 0;
-        String playAgain = "";
 
-        System.out.println("Welcome to Math Games!");
-        System.out.println("Select a game: ");
-        System.out.println("+ Addition");
-        System.out.println("- Subtraction");
-        System.out.println("* Multiplication");
-        System.out.println("/ Division");
+        System.out.println("What is your name?");
+        // userName = myScanner.nextLine();
+        userName = "CharMelia";
+        System.out.println("Hello!, " + userName + "! It is nice to meet you.");
+        System.out.println("Please choose a difficulty:");
+        System.out.println("1 = Add and Subtract");
+        System.out.println("2 = Multiplication");
+        System.out.println("3 = Multiplication and Division");
+        System.out.println("4 = Long Division");
+        difficultyLevel = myScanner.nextLine(); // Read user input
 
-        operator = scannerBoi.nextLine(); // Read user input
-
+        while (!difficultyLevel.equals("1") && !difficultyLevel.equals("2") && !difficultyLevel.equals("3") && !difficultyLevel.equals("4")) {
+            System.out.println("Invalid choice. Please type 1, 2, 3, or 4");
+            difficultyLevel = myScanner.nextLine();
+        }
+       
+        
         while (lives > 0 && correctAnswers < 10) {
             System.out.println(" ");
-            System.out.println("You chose option " + operator);
             System.out.println("You have " + lives + " lives left and " + correctAnswers + " correct answers.");
             System.out.println(" ");
-
-            switch (operator) {
-                case "+":
-                    number1 = (int) (Math.random() * 13); // 0 to 12
-                    number2 = (int) (Math.random() * 13); // 0 to 12
-                    answer = number1 + number2;
-                    System.out.println(number1 + " " + operator + " " + number2 + " = ");
-                    System.out.println("Submit your answer");
-                    userAnswer = Integer.parseInt(scannerBoi.nextLine());
-
-                    if (userAnswer == answer) {
-                        System.out.println("Correct");
-                        System.out.println(" ");
-                        ++correctAnswers;
-
-                    } else {
-                        --lives;
-                        System.out.println("Incorrect");
-                        System.out.println("The answer is " + answer);
-                        System.out.println(" ");
-
-                    }
-
+            
+            switch (difficultyLevel) {
+                case "1":
+                    addSubtract();
                     break;
-
-                case "-":
-                    number1 = (int) (Math.random() * 13); // 0 to 12
-                    number2 = (int) (Math.random() * 13); // 0 to 12
-                    answer = number1 + number2;
-                    System.out.println(answer + " " + operator + " " + number1 + " = ");
-                    System.out.println("Submit your answer");
-                    userAnswer = Integer.parseInt(scannerBoi.nextLine());
-
-                    if (userAnswer == number2) {
-                        System.out.println("Correct");
-                        System.out.println(" ");
-                        ++correctAnswers;
-
-                    } else {
-                        --lives;
-                        System.out.println("Incorrect");
-                        System.out.println("The answer is " + number2);
-                        System.out.println(" ");
-
-                    }
-
+    
+                case "2":
+                    multiplication();
                     break;
-
-                case "*":
-                    number1 = (int) (Math.random() * 13); // 0 to 12
-                    number2 = (int) (Math.random() * 13); // 0 to 12
-                    answer = number1 * number2;
-                    System.out.println(number1 + " " + operator + " " + number2 + " = ");
-                    System.out.println("Submit your answer");
-                    userAnswer = Integer.parseInt(scannerBoi.nextLine());
-
-                    if (userAnswer == answer) {
-                        System.out.println("Correct");
-                        System.out.println(" ");
-                        ++correctAnswers;
-
-                    } else {
-                        --lives;
-                        System.out.println("Incorrect");
-                        System.out.println("The answer is " + answer);
-                        System.out.println(" ");
-
-                    }
-
+    
+                case "3":
+                    multiplicationDivision();
                     break;
-
-                case "/":
-
-                    number1 = (int) (Math.random() * 12 + 1); // 1 to 12
-                    number2 = (int) (Math.random() * 12 + 1); // 1 to 12
-                    answer = number1 * number2;
-                    System.out.println(answer + " " + operator + " " + number1 + " = ");
-                    System.out.println("Submit your answer");
-                    userAnswer = Integer.parseInt(scannerBoi.nextLine());
-
-                    if (userAnswer == number2) {
-                        System.out.println("Correct");
-                        System.out.println(" ");
-                        ++correctAnswers;
-
-                    } else {
-                        --lives;
-                        System.out.println("Incorrect");
-                        System.out.println("The answer is " + number2);
-
-                    }
-
+    
+                case "4":
+                    longDivision();
                     break;
-
-                default:
-
-                    break;
-
+            
             }
+        }
+        
+        if (lives == 0) {
+            System.out.println("You ran out of lives");
+            restart();
+        } else if (correctAnswers == 10){
+            System.out.println("Great Job, " + userName + "! You got 10 correct. Keep up the good work.");
+            restart();
+        }
+    }
 
+    static void addSubtract(){
+        number1 = (int) (Math.random() * 13); // 0 to 12
+        number2 = (int) (Math.random() * 13); // 0 to 12
+        int operator = (int) (Math.random() * 10);
+        
+        if (operator > 5) { // + operator
+            answer = number1 + number2;
+            System.out.println(number1 + " + " + number2 + " = ");
+            userAnswer = Integer.parseInt(myScanner.nextLine());
+            System.out.println("The answer is " + answer);
+            if (userAnswer == answer) {
+                System.out.println("You are correct.");
+                correctAnswers ++;
+            } else {
+                System.out.println("You are incorrect.");
+                lives --;  
+            } 
+        } else {  // - operator
+            
+            number2 = answer - number1;
+            System.err.println(answer + " - " + number1 + " = ");
+            userAnswer = Integer.parseInt(myScanner.nextLine());
+            System.out.println("The answer is " + number2);
+            if (userAnswer == number2) {
+                System.out.println("You are correct.");
+                correctAnswers ++;
+            } else {
+                System.out.println("You are incorrect.");
+              lives --;  
+            } 
         }
 
     }
-
+    static void multiplication(){
+        System.out.println("test multiplication");        
+        lives --;
+    }
+    static void multiplicationDivision(){
+        System.out.println("test multiplication/division");        
+        lives --;
+    }
+    static void longDivision(){
+        System.out.println("test long divison");
+        lives --;
+    }
+    
+    static void restart(){
+        String playAgain;
+    
+        System.out.println("Do you want to play again? Y/N");
+        playAgain = myScanner.nextLine().toUpperCase();
+        System.out.println(playAgain);
+    
+        if (playAgain.equals("Y")) {
+            lives = 3;
+            correctAnswers = 0;
+            main(null);
+        } else if (playAgain.equals("N")) {
+            myScanner.close();
+            System.out.println("Goodbye!");
+        } else {
+            System.out.println("Invalid choice. Please choose \"Y\" or \"N\"");
+            restart();
+        };
+    }
 }
+
+//add function to write username/data to a file 
